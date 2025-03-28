3e80a566c6c49d8035f3b8ad5102d7779aefec11
sleimanhub <sleimanhassan69@gmail.com>
2025-03-28 17:39:26 +0200

    new commit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+  console.log(`App is listening on port ${PORT}...`);
 });
 
